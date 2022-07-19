830579e037ed9c0b7e104710d898c1f1c0a7a86c
Divyam Ahuja <ahujadivyam@gmail.com>
2022-07-19 15:52:17 +0530

    Update UI and README

--- js/script.js
+++ js/script.js
@@ -1,7 +1,42 @@
-let worker = new Worker(new URL('./worker.js', import.meta.url), { type: "module" });
+// ***** Tests *****
+
+let secureContext = window.isSecureContext;
+let crossOriginIsolated = window.crossOriginIsolated;
+let SharedArrayBufferAvailable = window.SharedArrayBuffer !== undefined
+let AudioWorkletAvailable = window.AudioWorklet !== undefined
+let WebAssemblyAvailable = window.WebAssembly !== undefined
 
 
 const toogle_button = document.getElementById('toogle');
+const checksTable = document.getElementById('checks');
+
+let checks = [
+    ["Secure Context", secureContext],
+    ["Cross Origin Isolated", crossOriginIsolated],
+    ["SharedArrayBuffer", SharedArrayBufferAvailable],
+    ["AudioWorklet", AudioWorkletAvailable],
+    ["WebAssembly", WebAssemblyAvailable]
+]
+
+checks.forEach(check => {
+    checksTable.innerHTML += `
+        <tr>
+            <td>${check[0]}</td>
+            <td>${check[1] ? "✔️" : "❌" }</td>
+        </tr>
+    `
+})
+
+if (checks.every(check => check[1])) {
+    toogle_button.disabled = false;
+}
+
+// *****************
+
+
+let worker = new Worker(new URL('./worker.js', import.meta.url), { type: "module" });
+
+
 let audioContext;
 let playing = false;
 
@@ -24,6 +59,11 @@ worker.onmessage = (msg) => {
                 audioContext.resume();
                 playing = true;
             }
+        }
+        if (playing) {
+            toogle_button.style.backgroundColor = "#00ff22";
+        } else {
+            toogle_button.style.backgroundColor = "#f35858";    
         }
     }
 }
