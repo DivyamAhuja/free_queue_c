26d9dcecf1eaba7088bdce714abd627a635b3d25
Divyam Ahuja <ahujadivyam@gmail.com>
2022-07-19 14:48:33 +0530

    Update Build Scripts

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 mkdir -p "build"
-emcc src/main.c -sENVIRONMENT=worker -sMODULARIZE=1 -sEXPORT_NAME=FQC -sINVOKE_RUN=0 -pthread -sEXPORTED_RUNTIME_METHODS="['callMain','ccall', 'cwrap']" -o build/main.js
+emcc src/main.c -sENVIRONMENT=worker -sMODULARIZE=1 -sEXPORT_NAME=FQC -sEXPORT_ES6=1 -sINVOKE_RUN=0 -pthread -sEXPORTED_RUNTIME_METHODS="['callMain','ccall', 'cwrap']" -o build/main.js
